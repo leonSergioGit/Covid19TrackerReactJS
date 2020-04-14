23871f83b1b7ce65dd2656cd332e44a8cc751512
unknown <leongit1990@gmail.com>
2020-04-14 15:13:52 +0200

    Charts and data finished

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
+import coronaImage from './images/image.png';
+
 class App extends React.Component {
     state = {
-        data: {}
+        data: {},
+        country: ''
     }
     
 
@@ -19,13 +22,20 @@ class App extends React.Component {
         this.setState({ data: fetchedData });
     }
 
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData, country: country })
+    }
+
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
         return (
             <div className={styles.container}>
+                <img src={coronaImage} className={styles.image} alt="COVID-19" />
                 <Cards data={ data }/>
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         )
     }
